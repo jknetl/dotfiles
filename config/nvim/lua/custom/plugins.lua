eb3217fa1b6c8e7043cc5bae4c1a37b3439a8d4d
Jakub Knetl <knetl.j@gmail.com>
2024-02-12 11:38:49 +0100

    Add custom neovim config

---
+++ config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,28 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  }
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
+}
+
+return plugins
