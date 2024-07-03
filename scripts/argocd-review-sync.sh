66ef79e0554c2c1f642f88967f5764fb7cf1a20a
Jakub Knetl <knetl.j@gmail.com>
2024-07-03 15:57:37 +0200

    Argo sync improvements

--- scripts/argocd-review-sync.sh
+++ scripts/argocd-review-sync.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-export KUBECTL_EXTERNAL_DIFF="git --no-pager diff -U0 --word-diff --word-diff-regex=."
+if [ -z "$KUBECTL_EXTERNAL_DIFF" ]; then
+  export KUBECTL_EXTERNAL_DIFF="git --no-pager diff -U1 --word-diff --word-diff-regex=."
+fi
+
 ARGOCD_OPTS="--grpc-web"
 # Function to list all apps matching the filter
 list_apps() {
-    local filter=$1
-    argocd $ARGOCD_OPTS app list -l "$filter" --output=name
+    local filter="$1"
+    argocd $ARGOCD_OPTS app list -l "$filter" --output=json | jq -r '.[] | select(.status.sync.status!="Synced") | .metadata.name'
 }
 
 print_apps() {
-    local filter=$1
-    local apps=$2
+    local filter="$1"
+    local apps="$2"
     echo "============================================================"
-    echo "List of applications for filter:"
+    echo "List of not synced applications for filter:"
     echo "$filter"
     echo "============================================================"
     for app in $apps; do
@@ -22,16 +25,22 @@ print_apps() {
 
 # Function to display diff for each app
 display_diff() {
-    local app=$1
+    local app="$1"
     argocd $ARGOCD_OPTS app diff "$app"
 }
 
 # Function to sync all apps matching the filter
 sync_apps() {
-    local filter=$1
-    echo "Syncing apps matching filter: $filter"
+    local filter="$1"
+    local apps="$2"
+
     sleep 2 # Safety sleep to allow to interrupt
-    argocd $ARGOCD_OPTS app sync -l "$filter"
+    echo "Syncing out of sync apps matching filter: $filter"
+
+    for app in $apps; do
+      argocd $ARGOCD_OPTS app sync "$app"
+    done
+
 }
 
 # Main script
@@ -41,13 +50,13 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-filter=$1
+filter="$1"
 apps=$(list_apps "$filter")
 
-print_apps $filter "$apps"
+print_apps "$filter" "$apps"
 
 if [ -z "$apps" ]; then
-    echo "No apps found matching the filter: $filter"
+    echo "No out of sync apps found matching the filter: $filter"
     exit 0
 fi
 
@@ -61,7 +70,7 @@ read -p "Do you want to apply the changes? (YES to proceed): " answer
 echo "============================================================"
 
 if [ "$answer" == "YES" ]; then
-  sync_apps "$filter"
+  sync_apps "$filter" "$apps"
 else
     echo "No changes applied."
     exit 0
