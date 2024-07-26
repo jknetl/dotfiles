64421e1fc7d07c34fcc4f19695ae50e9920853ff
Jakub Knetl <knetl.j@gmail.com>
2024-07-26 08:49:21 +0200

    Add script for switching AWS role in the current account

--- scripts/aws-role.sh
+++ scripts/aws-role.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-# Interactively chooses an AWS_PROFILE from the config using fzf
+# Interactively chooses an AWS_PROFILE from the aws based on currently used profile
+# It offers only the different role for the same account which is ucrrently active
+# If there is no active profile, it will show all the profiles
 
 # Path to the configuration file
 AWS_CONFIG_FILE=$HOME/.aws/config
 
-
 # Extract profile names from the config file
 options=($(sed -n 's/^\[profile \([^]]*\)\].*/\1/p' "$AWS_CONFIG_FILE"))
+
+current_profile=$AWS_PROFILE
+
+if [ ! -z "$current_profile" ]; then
+  # the current_profile starts has two parts separated by a '/' filter out options which have different first part
+  echo "$current_profile"
+  current_account=$(echo $current_profile | cut -d'/' -f1)
+  echo $current_account
+  options=($(printf "%s\n" "${options[@]}" | grep "$current_account"))
+fi
+
+
 # Check if options were found
 if [ ${#options[@]} -eq 0 ]; then
   echo "No profiles found in the configuration file."
