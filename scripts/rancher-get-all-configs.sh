d6317f2c895323f8047eb45a25e3c35130c64f2a
Jakub Knetl <knetl.j@gmail.com>
2024-09-25 11:59:05 +0200

    Parallelize getting kubeconfigs

--- scripts/rancher-get-all-configs.sh
+++ scripts/rancher-get-all-configs.sh
@@ -7,7 +7,9 @@ RANCHER_KUBECONFIG_FILE="${RANCHER_KUBECONFIG_FILE:=~/.kube/rancher-clusters.yam
 #echo "Clearing the currrent contnent of the kubeconfig file: $RANCHER_KUBECONFIG_FILE"
 #echo "" > $RANCHER_KUBECONFIG_FILE
 
-config_files=()
+
+FETCHED_CONFIG_FILES=$(mktemp -t rancher-kubeconfig-list)
+
 
 function get_config_from_rancher() {
     TEMPFILE=$(mktemp -t rancher-kubeconfig)
@@ -18,12 +20,29 @@ function get_config_from_rancher() {
 for cluster in $clusters; do
   cluster_name=$(echo $cluster | cut -d'/' -f1)
   cluster_id=$(echo $cluster | cut -d'/' -f2)
-  echo "Getting kubeconfig for cluster: $cluster ($cluster_id)"
-  config_file=$(get_config_from_rancher $cluster_name $cluster_id)
-  config_files+=($config_file)
+
+    {
+      echo "Getting kubeconfig for cluster: $cluster ($cluster_id)"
+      config_file=$(get_config_from_rancher $cluster_name $cluster_id)
+      echo $config_file >> $FETCHED_CONFIG_FILES
+    } &
+
+    ((counter++))
+    if [ $counter -ge 10 ]; then
+      wait
+      counter=0
+    fi
+
 done
 
+wait
+
+while IFS= read -r line; do
+  config_files+=("$line")
+done < $FETCHED_CONFIG_FILES
+rm $FETCHED_CONFIG_FILES
+
+echo "Merging to kubeconfig file..."
 for config_file in "${config_files[@]}"; do
-  echo "Merging kubeconfig file: $config_file"
-  kubeconfig-merge.sh -f $config_file
+  kubeconfig-merge.sh -f $config_file > /dev/null
 done
