0a018da8c8778ad24a2a66c1fa28610832fcf06a
Jakub Knetl <knetl.j@gmail.com>
2025-09-11 06:56:30 +0200

    Add yamldefscript

---
+++ scripts/yamldef.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script searches for a specific pattern definition in yaml files 
+# Usage example:
+# yamldef.sh <PATTERN>
+# e.g. yamldef.sh config.application.url
+
+PATTERN=$1
+if [ -z "$PATTERN" ]; then
+  echo "Usage: yamldef.sh <PATTERN>"
+  echo "Example: "
+  echo "yamldef.sh config.application.url"
+  exit 1
+fi
+
+yq "select(.${PATTERN}) | .${PATTERN} | (filename + \":\" + (line | tostring))" **/*.yaml
+
