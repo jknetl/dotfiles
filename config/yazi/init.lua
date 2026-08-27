bdfba53ca0904df4032925f225b31805e3b39992
Jakub Knetl <knetl.j@gmail.com>
2026-08-27 07:10:43 +0200

    Add linmode size and time to yazi

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,1 +1,16 @@
 require("folder-rules"):setup()
+
+-- Custom linemode combining size and modified time (used via `linemode = "size_and_mtime"` in yazi.toml)
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
